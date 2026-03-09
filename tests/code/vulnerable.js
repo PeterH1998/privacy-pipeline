322eb806d0bec6bb0198f626971f0529efc96d2a
PeterH1998 <46995405+PeterH1998@users.noreply.github.com>
2026-03-09 11:40:06 +0200

    CodeQL vulnerable tests

--- tests/code/vulnerable.js
+++ tests/code/vulnerable.js
@@ -1,4 +1,19 @@
+// vulnerable_command_injection.js
+const { exec } = require("child_process");
+
+function runCommand(userInput) {
+    exec(userInput, (error, stdout, stderr) => {
+        console.log(stdout);
+    });
+}
+
+runCommand(process.argv[2]);
+
+// vulnerable_eval.js
 const userInput = process.argv[2];
 
-// Vulnerable: executing user input
-eval(userInput);
+function executeInput(input) {
+    eval(input);
+}
+
+executeInput(userInput);
--- tests/code/vulnerable.py
+++ tests/code/vulnerable.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("test.db")
 cursor = conn.cursor()
 
-user = input("username: ")
-query = "SELECT * FROM users WHERE name='" + user + "'"
+username = input("Enter username: ")
 
+query = "SELECT * FROM users WHERE name = '" + username + "'"
 cursor.execute(query)
